b859f0e0c38b3a1c167a55847d31f853916a0693
Fliqqr <filip.duris.2@gmail.com>
2021-01-29 18:05:25 +0100

    test

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="asyncdrive", # Replace with your own username
-    version="1.1.2",
+    version="1.1.3",
     author="Fliqqr",
     author_email="author@example.com",
     description="",
