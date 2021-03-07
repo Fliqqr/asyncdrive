6fb5a9570a69e46849f398f0bec7713cf8b1b733
Fliqqr <filip.duris.2@gmail.com>
2021-03-07 21:44:13 +0100

    updated version number

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="asyncdrive", # Replace with your own username
-    version="1.2.0",
+    version="1.2.1",
     author="Fliqqr",
     author_email="author@example.com",
     description="",
